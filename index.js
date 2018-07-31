d4b6a43427779457d498ced63915b9a64044cf0c
ZeyadMamdouh <41742078+ZeyadMamdouh@users.noreply.github.com>
2018-07-31 23:39:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var prefix = "1"
 client.on('ready', function(){
     client.user.setStatus("online");
     var ms = 100000 ;
-    var setGame = [`1mhelp`,`Emerald Bot Shop ❤ ✯`];
+    var setGame = [`1mhelp`,`Bat Community ❤ ✯`];
     var i = -1;
     var j = 0;
     setInterval(function (){
